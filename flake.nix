78c7f74369755960986d2b4475211513d66c0e2d
Kevin Michaels <kmichaels@L-5CG4443W7F.localdomain>
2025-09-30 10:10:45 +1300

    chore: add fzf and man to nix devShell

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
         fastfetch
         fd
         fx
-        # fzf
+        fzf
         gcalcli
         git-credential-manager
         gitingest
@@ -71,6 +71,7 @@
         glow
         gtypist
         lazygit
+        man
         mermaid-cli
         mods 
         ocrmypdf
